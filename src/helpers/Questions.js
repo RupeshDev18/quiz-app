109e2358f7bef6134d375a68f9404a54d4cb9da0
Rupesh <112386480+Rupesh180902@users.noreply.github.com>
2023-07-22 20:11:33 +0530

    changed few questions

--- src/helpers/Questions.js
+++ src/helpers/Questions.js
@@ -1,16 +1,16 @@
 
 
 export const Questions=[{
-    prompt : "what is my name?",
-    optionA:'John',
-    optionB:'Josh',
-    optionC:'Jake',
-    optionD:'Joe',
-    answer:'optionD'
+    prompt : "Javascript is an _______ language?",
+    optionA:'Object-Oriented',
+    optionB:'Object-Based',
+    optionC:'Procedural',
+    optionD:'None',
+    answer:'optionA'
     },{
     prompt : "which of this is not programmimg Language?",
     optionA:'Python',
-    optionB:'JS',
+    optionB:'JavaScript',
     optionC:'Ruby',
     optionD:'Fortan',
     answer:'optionD'
@@ -21,6 +21,41 @@ export const Questions=[{
     optionC:'React',
     optionD:'Nextjs',
     answer:'optionC'
+    },{
+    prompt : "Which of the following keywords is used to define a variable in Javascript?",
+    optionA:'let',
+    optionB:'var',
+    optionC:'Both (a) and (b)',
+    optionD:'Nextjs',
+    answer:'optionC'
+    },{
+    prompt : "Which of the following methods is used to access HTML elements using Javascript?",
+    optionA:'getElementById()',
+    optionB:'getElementsByClassName()',
+    optionC:'Both a and b',
+    optionD:'None',
+    answer:'optionC'
+    },{
+    prompt : "How can a datatype be declared to be a constant type?",
+    optionA:'const',
+    optionB:'let',
+    optionC:'var',
+    optionD:'constant',
+    answer:'optionA'
+    },{
+    prompt : "When the switch statement matches the expression with the given labels, how is the comparison done?",
+    optionA:'Both the datatype and result of the expression are compared',
+    optionB:'Only the datatype is compared',
+    optionC:'Only value is compared',
+    optionD:'None',
+    answer:'optionA'
+    },{
+    prompt : "What is the use of the <noscript> tag in Javascript?",
+    optionA:'The contents are displayed by npn-js-based browsers',
+    optionB:'Clears all the cookies and cache',
+    optionC:'Both (a) and (b)',
+    optionD:'None',
+    answer:'optionA'
     }]
 
 /**
